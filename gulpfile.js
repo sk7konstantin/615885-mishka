941d2e891ecba1986cff451b49364d21ebb4bb13
Konstantin <sk7konstantin@yandex.ru>
2018-10-22 01:51:52 +0300

    Сделал оптимизацию изображений и минифицировал стили

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -14,6 +17,9 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
@@ -30,5 +36,16 @@ gulp.task("server", function () {
   gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 7}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.svgo()
+      ]))
+
+    .pipe(gulp.dest("source/img"))
+})
 
 gulp.task("start", gulp.series("css", "server"));
