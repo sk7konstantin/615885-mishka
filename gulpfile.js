cd0b8a8c09f67823e5f87e5ed7faf889bfd49db2
Konstantin <sk7konstantin@yandex.ru>
2018-10-22 11:41:05 +0300

    Подключил webp изображения

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -44,7 +45,12 @@ gulp.task("images", function() {
         imagemin.jpegtran({progressive: true}),
         imagemin.svgo()
       ]))
+    .pipe(gulp.dest("source/img"))
+})
 
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"))
 })
 
