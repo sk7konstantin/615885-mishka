6bb5dabfb36001dc4212f5c814619de3408625c7
Konstantin <sk7konstantin@yandex.ru>
2018-11-11 09:31:09 +0300

    Добавил слайдер

--- source/js/index.js
+++ source/js/index.js
@@ -2,7 +2,13 @@ var nav = document.querySelector('.nav');
 var navToggle = document.querySelector('.nav__toggle');
 var buttons = document.querySelectorAll(".product__button--js");
 var popup = document.querySelector(".modal-size");
+var prev = document.querySelector(".slider__toggle--prev");
+var next = document.querySelector(".slider__toggle--next");
+var slider = document.querySelectorAll(".reviews__item");
+var i = 0;
 var button;
+
+//Мобильное меню
 
 nav.classList.remove('nav--nojs');
 
@@ -34,4 +40,31 @@ window.addEventListener("keydown", function(evt) {
       popup.classList.remove("modal-size-show");
     }
   }
+})
+
+//Слайдер
+
+
+slider[i].classList.add("slider-show");
+
+prev.addEventListener('click', function() {
+  slider[i].classList.remove("slider-show");
+  i--;
+
+  if (i < 0) {
+    i = slider.length - 1;
+  }
+
+  slider[i].classList.add("slider-show");
+})
+
+next.addEventListener('click', function() {
+  slider[i].classList.remove("slider-show");
+  i++;
+
+  if (i >= slider.length) {
+    i = 0;
+  }
+
+  slider[i].classList.add("slider-show");
 })
